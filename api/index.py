acc6e0410b02ec07823b382f750b295243564a5b
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-02-02 13:24:36 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
-from sanic import Sanic
-from sanic.response import json
-app = Sanic()
+from http.server import BaseHTTPRequestHandler
  
+class handler(BaseHTTPRequestHandler):
  
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
